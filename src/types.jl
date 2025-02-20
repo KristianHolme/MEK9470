193f617619e818a3f8fc19f2a2b6361225f6a2be
Kristian Holme <holme.kristian@gmail.com>
2025-02-20 17:56:39 +0100

    beautyfix

--- src/types.jl
+++ src/types.jl
@@ -30,33 +30,33 @@ struct QUICKlimiter <: AbstractLimiter end
 function apply_limiter end
 
 function apply_limiter(limiter::VanLeer, r)
-    return (r + abs(r)) / (1 + abs(r))
+    (r + abs(r)) / (1 + abs(r))
 end
 
 function apply_limiter(limiter::VanAlbada, r)
-    return (r^2 + r) / (r^2 + 1)
+    (r^2 + r) / (r^2 + 1)
 end
 
 function apply_limiter(limiter::Minmod, r)
-    return max(0, min(1, r))
+    max(0, min(1, r))
 end
 
 function apply_limiter(limiter::Superbee, r)
-    return max(0, min(1, 2*r), min(2, r))
+    max(0, min(1, 2*r), min(2, r))
 end
 
 function apply_limiter(limiter::Sweby, r)
-    return max(0, min(limiter.β*r, 1), min(r, limiter.β))
+    max(0, min(limiter.β*r, 1), min(r, limiter.β))
 end
 
 function apply_limiter(limiter::UMIST, r)
-    return max(0, min(2r, (1+3r)/4, (3+r)/4, 2))
+    max(0, min(2r, (1+3r)/4, (3+r)/4, 2))
 end
 
 function apply_limiter(limiter::UD, r)
-    return 0.0
+    0.0
 end
 
 function apply_limiter(limiter::QUICKlimiter, r)
-    return max(0, min(2r, (3+r)/4, 2))
+    max(0, min(2r, (3+r)/4, 2))
 end
