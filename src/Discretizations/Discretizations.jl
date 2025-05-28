ba4e3af4b5967aa61d2285757baab815d82bbefe
Kristian Holme <holme.kristian@gmail.com>
2025-05-28 09:19:01 +0200

    rename folder and make limiter call to struct calls

--- src/Discretizations/Discretizations.jl
+++ src/Discretizations/Discretizations.jl
@@ -26,7 +26,7 @@ struct VanLeer <: AbstractLimiter end
 struct VanAlbada <: AbstractLimiter end
 struct Minmod <: AbstractLimiter end
 struct Superbee <: AbstractLimiter end
-struct Sweby <: AbstractLimiter 
+struct Sweby <: AbstractLimiter
     β::Float64
     function Sweby(β::Float64=1.5)
         return new(β)
@@ -35,38 +35,40 @@ end
 struct UMIST <: AbstractLimiter end
 struct QUICKlimiter <: AbstractLimiter end
 
-function apply_limiter end
+function apply_limiter(limiter::AbstractLimiter, r::Real)
+    limiter(r)
+end
 
-function apply_limiter(limiter::VanLeer, r)
+function (limiter::VanLeer)(r::Real)
     (r + abs(r)) / (1 + abs(r))
 end
 
-function apply_limiter(limiter::VanAlbada, r)
+function (limiter::VanAlbada)(r::Real)
     (r^2 + r) / (r^2 + 1)
 end
 
-function apply_limiter(limiter::Minmod, r)
+function (limiter::Minmod)(r::Real)
     max(0, min(1, r))
 end
 
-function apply_limiter(limiter::Superbee, r)
-    max(0, min(1, 2*r), min(2, r))
+function (limiter::Superbee)(r::Real)
+    max(0, min(1, 2 * r), min(2, r))
 end
 
-function apply_limiter(limiter::Sweby, r)
-    max(0, min(limiter.β*r, 1), min(r, limiter.β))
+function (limiter::Sweby)(r::Real)
+    max(0, min(limiter.β * r, 1), min(r, limiter.β))
 end
 
-function apply_limiter(limiter::UMIST, r)
-    max(0, min(2r, (1+3r)/4, (3+r)/4, 2))
+function (limiter::UMIST)(r::Real)
+    max(0, min(2r, (1 + 3r) / 4, (3 + r) / 4, 2))
 end
 
-function apply_limiter(limiter::UD, r)
+function (limiter::UD)(r::Real)
     zero(r)
 end
 
-function apply_limiter(limiter::QUICKlimiter, r)
-    max(0, min(2r, (3+r)/4, 2))
+function (limiter::QUICKlimiter)(r::Real)
+    max(0, min(2r, (3 + r) / 4, 2))
 end
 
 end
